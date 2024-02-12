aad22215ccc92687e71da002e7d722a251c8ca6b
Zikita Noteev <twinkl3work@gmail.com>
2024-02-12 17:04:57 +0300

    paginationFix

--- src/Features/DeleteBookButton/model/service/deteleBook.ts
+++ src/Features/DeleteBookButton/model/service/deteleBook.ts
@@ -7,16 +7,43 @@ export const remove = () => {
       cache.modify({
         fields: {
           allBooks(currentBooks = []) {
-              const result = currentBooks.find(
-                (obj: any) => {
-                  let parts = obj.__ref.split(":");
-                  let number = parseInt(parts[1]);
-                  return number % 10 === 1 && number !== 1
-                }
-              );
+            if (
+              currentBooks.length &&
+              currentBooks.length > Number(removeBook.id) - 1
+            ) {
+              const result = currentBooks.find((obj: any) => {
+                let parts = obj.__ref.split(":");
+                let number = parseInt(parts[1]);
+                return (
+                  number % 10 === 1 && number !== 1 && number > removeBook.id
+                );
+              });
               return currentBooks
                 .filter((book: any) => book.__ref !== `Book:${removeBook.id}`)
                 .push(result);
+            } else {
+              return currentBooks.filter(
+                (book: any) => book.__ref !== `Book:${removeBook.id}`
+              );
+            }
+            // }
+            // if (currentBooks.length === Number(removeBook.id) - 1) {
+            //   return currentBooks.filter(
+            //     (book: any) => book.__ref !== `Book:${removeBook.id}`
+            //   );
+            // } else {
+            //   const result = currentBooks.find((obj: any) => {
+            //     let parts = obj.__ref.split(":");
+            //     let number = parseInt(parts[1]);
+            //     return (
+            //       number % 10 === 1 && number !== 1 && number > removeBook.id
+            //     );
+            //   });
+            //   console.log(result);
+            //   return currentBooks
+            //     .filter((book: any) => book.__ref !== `Book:${removeBook.id}`)
+            //     .push(result);
+            // }
           },
           _allBooksMeta(allBooks) {
             return allBooks.count - 1;
